cee515a7365d57eb96b8de324fe1f331a51357ed
HariharanIT <ghariharanit@gmail.com>
2024-06-19 11:53:30 +0530

    Added changes for toolbar testing

--- conferencing/bottombar.tsx
+++ conferencing/bottombar.tsx
@@ -1,54 +1,22 @@
-import {ToolbarPreset, ToolbarComponents} from 'customization-api';
-import React from 'react';
+import { ToolbarPreset, ToolbarComponents } from "customization-api";
+import React from "react";
 
 const Bottombar = () => {
-  const {
-    MeetingTitleToolbarItem,
-    ParticipantCountToolbarItem,
-    InviteToolbarItem,
-    ChatToolbarItem,
-    ParticipantToolbarItem,
-  } = ToolbarComponents;
-
   return (
     <ToolbarPreset
       align="bottom"
       defaultItemsConfig={{
-        layout: {hide: 'yes'},
-        invite: {hide: 'yes'},
+        more: {
+          fields: {
+            caption: {
+              hide: "yes",
+            },
+            transcript: {
+              hide: "yes",
+            },
+          },
+        },
       }}
-      customItems={[
-        {
-          align: 'start',
-          component: MeetingTitleToolbarItem,
-          hide: 'no',
-          order: 0,
-        },
-        {
-          align: 'start',
-          component: ParticipantCountToolbarItem,
-          hide: 'no',
-          order: 1,
-        },
-        {
-          align: 'end',
-          component: ParticipantToolbarItem,
-          hide: 'no',
-          order: 1,
-        },
-        {
-          align: 'end',
-          component: ChatToolbarItem,
-          hide: 'no',
-          order: 2,
-        },
-        {
-          align: 'end',
-          component: InviteToolbarItem,
-          hide: 'no',
-          order: 3,
-        },
-      ]}
     />
   );
 };
--- conferencing/index.tsx
+++ conferencing/index.tsx
@@ -1,14 +1,12 @@
-import {customize, isMobileUA} from 'customization-api';
-import Topbar from './topbar';
-import Bottombar from './bottombar';
+import { customize } from "customization-api";
+import Bottombar from "./bottombar";
 
 const config = customize({
   components: {
     videoCall: {
-      topToolBar: Topbar,
       bottomToolBar: Bottombar,
     },
   },
 });
 
-export default isMobileUA() ? {} : config;
+export default config;
